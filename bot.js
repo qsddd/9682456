12b2515e2f03dc7e5cd8e6f4c43d7c7e2190cccb
qsddd <40525582+qsddd@users.noreply.github.com>
2018-06-24 02:45:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,6 @@ var prefix = ayarlar.prefix;
 const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
-
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./komutlar/', (err, files) => {
@@ -20,13 +19,13 @@ fs.readdir('./komutlar/', (err, files) => {
   log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {
     let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
     });
   });
 });
-
 client.reload = command => {
   return new Promise((resolve, reject) => {
     try {
@@ -46,7 +45,6 @@ client.reload = command => {
     }
   });
 };
-
 client.load = command => {
   return new Promise((resolve, reject) => {
     try {
@@ -61,7 +59,6 @@ client.load = command => {
     }
   });
 };
-
 client.unload = command => {
   return new Promise((resolve, reject) => {
     try {
@@ -76,7 +73,7 @@ client.unload = command => {
       reject(e);
     }
   });
-};
+    };
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
