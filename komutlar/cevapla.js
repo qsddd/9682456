dfa24447e3dc07a32c904554b75ea34a951d5ebf
qsddd <40525582+qsddd@users.noreply.github.com>
2018-06-25 16:31:37 +0300

    Create cevapla.js

---
+++ komutlar/cevapla.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+    if(message.author.id === "380753087012405249") {
+        let mesaj = args.slice(0).join(' ');
+        if (mesaj.length < 1) return message.channel.send(':no_entry: Gondermek Isdediniz Mesak?; `f?cevapla Tesekkurler`');
+        const embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setDescription('')
+        .setFooter('Fredux Tavsiye Cevaplama')
+        message.author.send(embed)
+    } else {
+        message.channel.send(':no_entry: Bu Komut Developer Kodudur.');
+    }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'cevapla',
+  description: 'cevapla',
+  usage: 'cevapla'
+};
