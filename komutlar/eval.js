5b38dc3c8677803a177472197a517057f2df29f7
qsddd <40525582+qsddd@users.noreply.github.com>
2018-06-25 17:37:58 +0300

    Update eval.js

--- komutlar/eval.js
+++ komutlar/eval.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
-	if (message.author.id === "311535278949007361") {
+	if (message.author.id === "380753087012405249") {
 		try {
 		  var code = args.join(" ");
 		  var evaled = eval(code);
