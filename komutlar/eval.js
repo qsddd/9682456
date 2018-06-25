fcb94d9ae60b178c540fb00543459f02b2ed3402
qsddd <40525582+qsddd@users.noreply.github.com>
2018-06-25 17:29:31 +0300

    Update eval.js

--- komutlar/eval.js
+++ komutlar/eval.js
@@ -2,101 +2,34 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
-  if (args.script.includes("token")) return msg.reply('bak şu an gördün mü tokeni?')
-		// Make a bunch of helpers
-		/* eslint-disable no-unused-vars */
-		const message = msg;
-		const client = msg.client;
-		const objects = client.registry.evalObjects;
-		const lastResult = this.lastResult;
-		const doReply = val => {
-			if(val instanceof Error) {
-				msg.reply(`Callback error: \`${val}\``);
-			} else {
-				const result = this.makeResultMessages(val, process.hrtime(this.hrStart));
-				if(Array.isArray(result)) {
-					for(const item of result) {
-						if(this.client.options.selfbot) msg.say(item); else msg.reply(item);
-					}
-				} else if(this.client.options.selfbot) {
-					msg.say(result);
-				} else {
-					msg.reply(result);
-				}
-			}
-		};
-		let hrDiff;
-		try {
-			const hrStart = process.hrtime();
-			this.lastResult = eval(args.script);
-			hrDiff = process.hrtime(hrStart);
-		} catch(err) {
-			return msg.reply(`Error while evaluating: \`${err}\``);
-		}
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	if(command === 'eval') {
+		 const code = args.join(" ");
+  const token = client.token.split("").join("[^]{0,2}");
+  const rev = client.token.split("").reverse().join("[^]{0,2}");
+  const filter = new RegExp(`${token}|${rev}`, "g");
+  try {
+    let output = eval(code);
+    if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
+    output = inspect(output, { depth: 0, maxArrayLength: null });
+    output = output.replace(filter, "[TOKEN]");
+    output = clean(output);
+    if (output.length < 1950) {
+      message.channel.send(`\`\`\`js\n${output}\n\`\`\``);
+    } else {
+    	  message.channel.send(`${output}`, {split:"\n", code:"js"});
+    }
+  } catch (error) {
+    message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``);
+ }
 
-		// Prepare for callback time and respond
-		this.hrStart = process.hrtime();
-		let response = this.makeResultMessages(this.lastResult, hrDiff, args.script, msg.editable);
-		if(msg.editable) {
-			if(response instanceof Array) {
-				if(response.length > 0) response = response.slice(1, response.length - 1);
-				for(const re of response) msg.say(re);
-				return null;
-			} else {
-				return msg.edit(response);
-			}
-		} else {
-			return msg.reply(response);
-		}
-	}
-
-	makeResultMessages(result, hrDiff, input = null, editable = false) {
-		const inspected = util.inspect(result, { depth: 0 })
-			.replace(nlPattern, '\n')
-			.replace(this.sensitivePattern, '[GIZLI]');
-		const split = inspected.split('\n');
-		const last = inspected.length - 1;
-		const prependPart = inspected[0] !== '{' && inspected[0] !== '[' && inspected[0] !== "'" ? split[0] : inspected[0];
-		const appendPart = inspected[last] !== '}' && inspected[last] !== ']' && inspected[last] !== "'" ?
-			split[split.length - 1] :
-			inspected[last];
-		const prepend = `\`\`\`javascript\n${prependPart}\n`;
-		const append = `\n${appendPart}\n\`\`\``;
-		if(input) {
-			return discord.splitMessage(tags.stripIndents`
-				${editable ? `
-					*Input*
-					\`\`\`javascript
-					${input}
-					\`\`\`` :
-				''}
-				*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
-				\`\`\`javascript
-				${inspected}
-				\`\`\`
-			`, 1900, '\n', prepend, append);
-		} else {
-			return discord.splitMessage(tags.stripIndents`
-				*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
-				\`\`\`javascript
-				${inspected}
-				\`\`\`
-			`, 1900, '\n', prepend, append);
-		}
-	}
-
-	get sensitivePattern() {
-		if(!this._sensitivePattern) {
-			const client = this.client;
-			let pattern = '';
-			if(client.token) pattern += escapeRegex(client.token);
-			if(client.email) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.email);
-			if(client.password) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.password);
-			Object.defineProperty(this, '_sensitivePattern', { value: new RegExp(pattern, 'gi') });
-		}
-		return this._sensitivePattern;
-	}
-
+  function clean(text)  {
+    return text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203));
+   }
 };
 
 exports.conf = {
